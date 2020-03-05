d54905ca2680ea860d793bc5005e334bb17c5d64
Gagik Papikyan <gagik787@gmail.com>
2020-03-05 09:10:42 +0300

    var name fix

--- src/utils.js
+++ src/utils.js
@@ -15,7 +15,7 @@ function clear(cvs){
     ctx.fillRect(0,0,cvs.width,cvs.height);    
 }
 
-function plot(cvs,src,range,ops = {}){
+function plot(cvs,src,domain,ops = {}){
     let ctx = cvs.getContext('2d');
     ctx.translate(cvs.width/2,cvs.height/2);
     ctx.beginPath();
@@ -24,14 +24,14 @@ function plot(cvs,src,range,ops = {}){
     if(typeof src === 'function'){
         let fn = src;
         let first = true;
-        for(let i = range[0];i<=range[1];i+=.01){
+        for(let i = domain[0];i<=domain[1];i+=.01){
             let x,y, res = fn(i);
 
             if(!Array.isArray(res)) res = [i,res];
             if(res[0]===null || res[1]===null) continue;
 
-            x = res[0] * cvs.width/range[1];
-            y = res[1] * cvs.width/range[1];
+            x = res[0] * cvs.width/domain[1];
+            y = res[1] * cvs.width/domain[1];
 
             if(first){ ctx.moveTo(x,y); first = false; }
             else ctx.lineTo(x,y);
@@ -44,8 +44,8 @@ function plot(cvs,src,range,ops = {}){
         for(let i = 0;i<tbl.length;i++){
             let x = tbl[i][0], y = tbl[i][1];
 
-            x = x * cvs.width/range[1];
-            y = y * cvs.width/range[1];
+            x = x * cvs.width/domain[1];
+            y = y * cvs.width/domain[1];
 
             if(first){ ctx.moveTo(x,y); first = false; }
             else ctx.lineTo(x,y);
