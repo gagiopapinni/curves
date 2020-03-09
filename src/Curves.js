6c84d7c9359af19805bc21d243e24e04e81d2300
Alex Vakhtin <vkstrfrt@gmail.com>
2020-03-09 23:57:59 +0300

    do it booy

--- src/Curves.js
+++ src/Curves.js
@@ -1,102 +1,142 @@
-const Curves = (function (){
-    const {pow, sin, cos, asin, sqrt, sign, tan} = Math; 
- 
-    function EvoluteFor(curve){ 
-       return (s)=>{
-           let c = curve,
-               dot = c.dot(s),
-               dotdot = c.dotdot(s),
-               res = c(s); 
+const Curves = (function () {
+        const {pow, sin, cos, asin, sqrt, sign, tan} = Math;
 
-           res[0] -= dot[1]*(pow(dot[0],2)+pow(dot[1],2))/(dot[0]*dotdot[1]-dot[1]*dotdot[0]);
-           res[1] += dot[0]*(pow(dot[0],2)+pow(dot[1],2))/(dot[0]*dotdot[1]-dot[1]*dotdot[0]);
-           return res;
-       } 
-    }
+        function EvoluteFor(curve) {
+            return (s) => {
+                let c = curve,
+                    derivative = c.derivative(s),
+                    secondDerivative = c.secondDerivative(s),
+                    res = c(s);
 
-    function Anguinea(t,a=.4,d=.4){
-       if(t<-Math.PI || t>Math.PI) return [null,null];
-       return [d*tan(t/2),a/2*sin(t)];
-    }
-    Anguinea.dot = function(t,a=.4,d=.4){
-       if(t<-Math.PI || t>Math.PI) return [null,null];
-       return [d/(2*pow(cos(t/2),2)), a/2*cos(t)];
-    }
-    Anguinea.dotdot = function(t,a=.4,d=.4){
-       if(t<-Math.PI || t>Math.PI) return [null,null];
-       return [1/2*d*1/pow(cos(t/2),2)*tan(t/2), -a/2*sin(t)];
-    }
-    Anguinea.evolute = EvoluteFor(Anguinea);
-    Anguinea.domain = [-Math.PI,Math.PI];
+                res.x -= derivative.y * (pow(derivative.x, 2) + pow(derivative.y, 2)) / (derivative.x * secondDerivative.y - derivative.y * secondDerivative.x);
+                res.y += derivative.x * (pow(derivative.x, 2) + pow(derivative.y, 2)) / (derivative.x * secondDerivative.y - derivative.y * secondDerivative.x);
+                return res;
+            }
+        }
 
- 
+        function Anguinea(t, a = .4, d = .4) {
+            if (t < -Math.PI || t > Math.PI) return [null, null];
+            return {
+                x: d * tan(t / 2),
+                y: a / 2 * sin(t)
+            }
+        }
 
-    function Circle(t,r=1){
-       return [r*sin(t),r*cos(t)];   
-    }
-    Circle.dot = function (t,r=1){
-       return [r*cos(t),-r*sin(t)];   
-    }
-    Circle.dotdot = function (t,r=1){
-       return [-r*sin(t),-r*cos(t)];   
-    }
-    Circle.evolute = EvoluteFor(Circle);
-    Circle.domain = [0,2*Math.PI];
+        Anguinea.derivative = function (t, a = .4, d = .4) {
+            if (t < -Math.PI || t > Math.PI) return [null, null];
+            return {
+                x: d / (2 * pow(cos(t / 2), 2)),
+                y: a / 2 * cos(t)
+            }
+        }
+        Anguinea.secondDerivative = function (t, a = .4, d = .4) {
+            if (t < -Math.PI || t > Math.PI) return {x: null, y: null};
+            return {
+                x: 1 / 2 * d * 1 / pow(cos(t / 2), 2) * tan(t / 2),
+                y: -a / 2 * sin(t)
+            };
+        }
+        Anguinea.evolute = EvoluteFor(Anguinea);
+        Anguinea.domain = [-Math.PI, Math.PI];
 
 
-    function Ellipse(t){
-       return [1.5*sin(t),1*cos(t)];   
-    }
+        function Circle(t, r = 1) {
+            return {
+                x: r * sin(t),
+                y: r * cos(t)
+            }
+        }
 
-    Ellipse.dot = function (t){
-       return [1.5*cos(t),-sin(t)];   
-    }
-
-    Ellipse.dotdot = function (t){
-       return [-1.5*sin(t),-cos(t)];   
-    }
-    Ellipse.evolute = EvoluteFor(Ellipse);
-    Ellipse.domain = [0,2*Math.PI];
-
-
-    function Cardioid (s){
-       return [ (2-2*pow(1-s/4,2))*(2*pow(1-s/4,2)-1), (2-2*pow(1-s/4,2))*2*(1-s/4)*pow(s/4*(2-s/4),1/2) ];
-    }
-    Cardioid.dot = function (s){
-       return [ -(s*s*s)/16 + 3*s*s/4 - 9*s/4 + 1, 1/16 * pow(-(s-8)*s,1/2) * (s*s-8*s+12) ];
-    }
-    Cardioid.dotdot = function (s){
-       return [ -3/16*(s*s-8*s+12), 3*(4-s)*(s*s-8*s+4)/(16*pow(-(s-8)*s,1/2))  ];
-    }
-    Cardioid.evolute = EvoluteFor(Cardioid);
-    Cardioid.domain = [0,8];
+        Circle.derivative = function (t, r = 1) {
+            return {
+                x: r * cos(t),
+                y: -r * sin(t)
+            }
+        }
+        Circle.secondDerivative = function (t, r = 1) {
+            return {
+                x: -r * sin(t),
+                y: -r * cos(t)
+            }
+        }
+        Circle.evolute = EvoluteFor(Circle);
+        Circle.domain = [0, 2 * Math.PI];
 
 
-    function Astroid (t,a = 1){
-       let x =  -a*pow(cos(t),3);
-       let y =  a*pow(sin(t),3);
-       return [x,y];
-    }
-    Astroid.dot = function (t,a = 1){
-       let x = 3*a*pow(cos(t),2)*sin(t);
-       let y =  3*a*cos(t)*pow(sin(t),2);
-       return [x,y];
-    }
-    Astroid.dotdot = function (t,a = 1){
-       let x = 3*a*cos(t)*(cos(t)*cos(t) - 2*sin(t)*sin(t));
-       let y = 6*a*cos(t)*cos(t)*sin(t) - 3*a*sin(t)*sin(t)*sin(t);
-       return [x,y];
-    }
-    Astroid.evolute = EvoluteFor(Astroid);
-    Astroid.domain = [0,6.3];
+        function Ellipse(t) {
+            return {
+                x: 1.5 * sin(t),
+                y: 1 * cos(t)
+            }
+        }
+
+        Ellipse.derivative = function (t) {
+            return {
+                x: 1.5 * cos(t),
+                y: -sin(t)
+            }
+        }
+
+        Ellipse.secondDerivative = function (t) {
+            return {
+                x: -1.5 * sin(t),
+                y: -cos(t)
+            };
+        }
+        Ellipse.evolute = EvoluteFor(Ellipse);
+        Ellipse.domain = [0, 2 * Math.PI];
 
 
-    return  {
-         Astroid: Astroid,
-         Circle: Circle,
-         Ellipse:Ellipse,
-         Anguinea:Anguinea,
-         Cardioid: Cardioid,
-    }
+        function Cardioid(s) {
+            return {
+                x: (2 - 2 * pow(1 - s / 4, 2)) * (2 * pow(1 - s / 4, 2) - 1),
+                y: (2 - 2 * pow(1 - s / 4, 2)) * 2 * (1 - s / 4) * pow(s / 4 * (2 - s / 4), 1 / 2)
+            }
+        }
 
-})()
+        Cardioid.derivative = function (s) {
+            return {
+                x: -(s * s * s) / 16 + 3 * s * s / 4 - 9 * s / 4 + 1,
+                y: 1 / 16 * pow(-(s - 8) * s, 1 / 2) * (s * s - 8 * s + 12)
+            }
+        }
+        Cardioid.secondDerivative = function (s) {
+            return {
+                x: -3 / 16 * (s * s - 8 * s + 12),
+                y: 3 * (4 - s) * (s * s - 8 * s + 4) / (16 * pow(-(s - 8) * s, 1 / 2))
+            }
+        }
+        Cardioid.evolute = EvoluteFor(Cardioid);
+        Cardioid.domain = [0, 8];
+
+
+        function Astroid(t, a = 1) {
+            let x = -a * pow(cos(t), 3);
+            let y = a * pow(sin(t), 3);
+            return {x, y};
+        }
+
+        Astroid.derivative = function (t, a = 1) {
+            let x = 3 * a * pow(cos(t), 2) * sin(t);
+            let y = 3 * a * cos(t) * pow(sin(t), 2);
+            return {x, y};
+        }
+        Astroid.secondDerivative = function (t, a = 1) {
+            let x = 3 * a * cos(t) * (cos(t) * cos(t) - 2 * sin(t) * sin(t));
+            let y = 6 * a * cos(t) * cos(t) * sin(t) - 3 * a * sin(t) * sin(t) * sin(t);
+            return {x, y};
+        }
+        Astroid.evolute = EvoluteFor(Astroid);
+        Astroid.domain = [0, 6.3];
+
+
+        return {
+            Astroid: Astroid,
+            Circle: Circle,
+            Ellipse: Ellipse,
+            Anguinea: Anguinea,
+            Cardioid: Cardioid,
+        }
+
+    }
+)();
--- src/index.js
+++ src/index.js
@@ -1,79 +1,96 @@
-$(window).on('load',start);
+$(window).on('load', start);
 
 let colors = {
-    curve:'red',
-    evolute:'grey',
-    reper:'grey',
-    osc_circ:'orange',
-    
+    curve: 'red',
+    evolute: 'grey',
+    reper: 'grey',
+    osc_circ: 'orange',
+};
+var currentCurve;
 
-}
 
-function start(){
+function start() {
     let cvs1 = $('#cvs1')[0],
         cvs2 = $('#cvs2')[0];
-    let curve = Curves[$('#curves').val()];
-    plot(cvs1,curve,curve.domain,{color:colors.curve});
-    plot(cvs2,curve,curve.domain,{color:colors.curve});
-    plot(cvs2,curve.evolute,curve.domain,{color:colors.evolute});
+    let curve = currentCurve = Curves[$('#curve_selector').val()];
+    cvs1.draw = function () {
+        multiDraw(cvs1, ...arguments);
+    };
+    cvs2.draw = function () {
+        multiDraw(cvs2, ...arguments);
+    };
+    const options = {
+        get curve() {
+            return [curve, {color: colors.curve}]
+        },
+        get evolute() {
+            return [curve.evolute, {color: colors.evolute}]
+        }
+    };
 
-    $('#curves').on("input",()=>{
-       curve = Curves[$('#curves').val()];
+    cvs1.draw(options.curve);
+    cvs2.draw(options.curve, options.evolute);
 
-       clear(cvs1)
-       plot(cvs1,curve,curve.domain,{color:colors.curve});
+    $('#curve_selector').on("input", () => {
+        curve = currentCurve = Curves[$('#curve_selector').val()];
+        clear(cvs1, cvs2);
+        cvs1.draw(options.curve);
 
-       clear(cvs2);
-       plot(cvs2,curve,curve.domain,{color:colors.curve});
-       plot(cvs2,curve.evolute,curve.domain,{color:colors.evolute});
-    })
+        cvs2.draw(options.curve, options.evolute);
+    });
 
-    $('#inp1').on('input',(e)=>{
-       let v = $("#inp1").val();
+    $('#state_slider').on('input', (e) => {
+        let v = $("#state_slider").val();
 
-       clear(cvs1);
-       plot(cvs1,curve,curve.domain,{color:colors.curve});
+        clear(cvs1);
+        cvs1.draw(options.curve);
 
-       let t = curve.domain[0] + (curve.domain[1]-curve.domain[0])*v;
-       let current_point = curve(t);
+        let t = curve.domain[0] + (curve.domain[1] - curve.domain[0]) * v;
+        let current_point = curve(t);
 
-       //unit tangent vector
-       let tangent = Vector.normalize( curve.dot(t) );
-       tangent = Vector.add( tangent, current_point );
-       plot(cvs1,[ current_point, tangent ],curve.domain,{color:colors.reper});
+        //unit tangent vector
+        let tangent = Vector.normalize(curve.derivative(t));
+        tangent = Vector.add(tangent, current_point);
+        cvs1.draw([[current_point, tangent], {color: colors.reper}]);
 
-       //unit normal vector
-       tangent = Vector.normalize( curve.dot(t) );
-       let normal = [-tangent[1],tangent[0]];
-       normal = Vector.add(normal,current_point);
-       plot(cvs1,[ current_point, normal ],curve.domain,{color:colors.reper});
+        //unit normal vector
+        tangent = Vector.normalize(curve.derivative(t));
+        let normal = {
+            x: -tangent.y,
+            y: tangent.x
+        };
+        normal = Vector.add(normal, current_point);
+        cvs1.draw([[current_point, normal], {color: colors.reper}]);
 
-       //evolute
-       clear(cvs2);
-       plot(cvs2,curve,curve.domain);
-       plot(cvs2,curve.evolute,curve.domain,{color:colors.evolute});
+        //evolute
+        clear(cvs2);
+        cvs2.draw(options.curve, options.evolute);
 
-       //osculating circle
-       let osc_circle_center = curve.evolute(t),
-           osc_circle_r = Vector.dist(current_point,osc_circle_center);
+        //osculating circle
+        let osc_circle_center = curve.evolute(t),
+            osc_circle_r = Vector.dist(current_point, osc_circle_center),
+            osc_curve = (s) => {
+                return {
+                    x: osc_circle_r * Math.sin(s) + osc_circle_center.x,
+                    y: osc_circle_r * Math.cos(s) + osc_circle_center.y
+                }
+            },
+            osc_circle = [osc_curve, {color: colors.osc_circ}],
+            vector_radius = [[current_point, curve.evolute(t)], {color: colors.osc_circ}];
 
-       plot(cvs2,(s)=>{return [osc_circle_r*Math.sin(s)+osc_circle_center[0], 
-                               osc_circle_r*Math.cos(s)+osc_circle_center[1]] },curve.domain,{color:colors.osc_circ});
+        cvs2.draw(osc_circle, vector_radius);
+    });
 
-       plot(cvs2,[ current_point,curve.evolute(t) ],curve.domain,{color:colors.osc_circ});
-
-    })
-
-    $('#anim').on('click',()=>{
-           $( "#inp1" ).val(0);
-           $({ value: 0 }).animate({ value: 1 },{
-             duration: 3000,
-             easing: 'linear',
-             step: value=>{
-               $("#inp1").val(value).trigger('input')
-             },
-             complete:()=>$( "#inp1" ).val(0),
-           });
+    $('#start_anim').on('click', () => {
+        $("#state_slider").val(0);
+        $({value: 0}).animate({value: 1}, {
+            duration: 3000,
+            easing: 'linear',
+            step: value => {
+                $("#state_slider").val(value).trigger('input')
+            },
+            complete: () => $("#state_slider").val(0),
+        });
     })
 }
 
